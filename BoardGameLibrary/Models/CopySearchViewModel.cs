4159596db6fd6c2e515aedf19e63d741463a8351
jjh456@gmail.com <jjh456@gmail.com>
2015-03-01 15:08:23 -0600

    Error message updates.

--- BoardGameLibrary/Models/CopySearchViewModel.cs
+++ BoardGameLibrary/Models/CopySearchViewModel.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using FluentValidation;
+using FluentValidation.Attributes;
 
 namespace BoardGameLibrary.Models
 {
+    [Validator(typeof(CopySearchValidator))]
     public class CopySearchViewModel
     {
         [Display(Name = "Library ID #")]
@@ -14,5 +17,17 @@ namespace BoardGameLibrary.Models
         public string AttendeeBadgeID { get; set; }
         [Display(Name = "Game Title")]
         public string GameTitle { get; set; }
+    }
+
+    public class CopySearchValidator : AbstractValidator<CopySearchViewModel>
+    {
+        ApplicationDbContext _db;
+
+        public CopySearchValidator()
+        {
+            _db = new ApplicationDbContext();
+            RuleFor(x => x.GameTitle).Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("You must provide search criteria.");
+        }
     }
 }
