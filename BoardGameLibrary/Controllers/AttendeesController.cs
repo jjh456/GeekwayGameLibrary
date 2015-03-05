0d70c423ec91efcaf3aa4dac398dc0c5a3e56967
jjh456@gmail.com <jjh456@gmail.com>
2015-03-05 10:06:18 -0600

    Style tweaks. Added import buttons to the games and attendees index views.

--- BoardGameLibrary/Controllers/AttendeesController.cs
+++ BoardGameLibrary/Controllers/AttendeesController.cs
@@ -86,6 +86,11 @@ namespace BoardGameLibrary.Controllers
             return View(Attendee);
         }
 
+        public async Task<ActionResult> Import()
+        {
+            throw new NotImplementedException();
+        }
+
         // GET: Attendees/Delete/5
         public async Task<ActionResult> Delete(int? id)
         {
