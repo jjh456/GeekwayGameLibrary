6d860c96f0a7e65b0ae9cc72198e273ceaf5e15b
jjh456@gmail.com <jjh456@gmail.com>
2015-05-04 23:04:48 -0500

    Can close sections on home page Focus first text box on check in/out. Uncheck override on check out

--- BoardGameLibrary/Scripts/geekway-library.js
+++ BoardGameLibrary/Scripts/geekway-library.js
@@ -2,12 +2,15 @@
     $('#check-out-form-wrapper input:text').each(function () {
         $(this).val("");
     });
+    $("#check-out-form-wrapper input:checkbox").removeAttr("checked");
+    $('#check-out-form-wrapper #CopyLibraryID').focus();
 }
 
 function clearCheckInForm() {
     $('#check-in-form-wrapper input:text').each(function () {
         $(this).val("");
     });
+    $('#check-in-form-wrapper #CopyLibraryID').focus();
 }
 
 function updateCheckOutForm(data) {
