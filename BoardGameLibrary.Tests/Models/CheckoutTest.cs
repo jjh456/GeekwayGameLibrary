739ce5eb2ea9e4e378ff694891606d665cc687f0
jjh456@gmail.com <jjh456@gmail.com>
2016-04-22 19:38:56 -0500

    Test checkout model length property calculation.

---
+++ BoardGameLibrary.Tests/Models/CheckoutTest.cs
@@ -0,0 +1,40 @@
+﻿using BoardGameLibrary.Models;
+using NUnit.Framework;
+using System;
+
+namespace BoardGameLibrary.Tests.Models
+{
+    [TestFixture]
+    public class CheckoutTest
+    {
+        [Test]
+        public void UnresolvedCheckoutLengthIsNowMinusTimeOut()
+        {
+            var timeOut = new DateTime(2016, 4, 22);
+            var model = new Checkout
+            {
+                TimeOut = timeOut
+            };
+
+            var expectedLength = DateTime.Now - timeOut;
+
+            Assert.AreEqual(expectedLength, model.Length);
+        }
+
+        [Test]
+        public void ResolvedCheckoutLengthIsTimeInMinusTimeOut()
+        {
+            var timeOut = new DateTime(2016, 4, 22);
+            var timeIn = new DateTime(2016, 4, 24);
+            var model = new Checkout
+            {
+                TimeOut = timeOut,
+                TimeIn = timeIn
+            };
+
+            var expectedLength = timeIn - timeOut;
+
+            Assert.AreEqual(expectedLength, model.Length);
+        }
+    }
+}
