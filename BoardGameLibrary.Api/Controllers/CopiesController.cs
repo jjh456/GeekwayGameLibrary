40a9c59a346e105321fb878fdecc93336943cc36
Joe Henderson <jjh456@gmail.com>
2018-02-09 20:59:48 -0600

    Return bad request and add model error if copy cannot be found by ID

--- BoardGameLibrary.Api/Controllers/CopiesController.cs
+++ BoardGameLibrary.Api/Controllers/CopiesController.cs
@@ -1,5 +1,6 @@
 ﻿using BoardGameLibrary.Api.Models;
 using BoardGameLibrary.Data.Models;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,9 +17,45 @@ namespace BoardGameLibrary.Api.Controllers
             _db = new ApplicationDbContext();
         }
 
+        public async Task<IHttpActionResult> Get(string query)
+        {
+            var copies = new List<Copy>();
+            if (query != null)
+            {
+                int searchedID = -1;
+                bool isNumeric = false;
+                try
+                {
+                    isNumeric = int.TryParse(query, out searchedID);
+                }
+                catch
+                {
+                    System.Console.WriteLine("Couldn't parse int");
+                }
+                var copiesMatchingTitle = await _db.Copies.Where(c => c.Game.Title.Contains(query)).ToListAsync();
+                var copiesMatchingID = new List<Copy>();
+                if (isNumeric && searchedID != -1)
+                {
+                    copiesMatchingID = await _db.Copies.Where(c => c.LibraryID == searchedID).ToListAsync();
+                    copies.AddRange(copiesMatchingID);
+                }
+
+                copies.AddRange(copiesMatchingTitle);
+                copies = copies.Distinct().ToList();
+            }
+            var copyResponseModels = copies.Select(c => new CopyResponseModel(c));
+
+            return Ok(copyResponseModels);
+        }
+
         public async Task<IHttpActionResult> Get(int id)
         {
             var copy = await _db.Copies.FirstOrDefaultAsync(c => c.LibraryID == id);
+            if (copy == null)
+            {
+                ModelState.AddModelError("id", "Couldn't find a copy with that ID");
+                return NotFound();
+            }
 
             return Ok(new CopyResponseModel(copy));
         }
