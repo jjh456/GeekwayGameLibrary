b3ac0497bf5b19da0804ace6a5641185717a43d9
jjh456 <jjh456@gmail.com>
2019-05-04 00:11:33 -0500

    require libraryID on creating a copy

--- BoardGameLibrary.Api/Models/UpsertCopyRequestModel.cs
+++ BoardGameLibrary.Api/Models/UpsertCopyRequestModel.cs
@@ -1,8 +1,11 @@
-﻿namespace BoardGameLibrary.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoardGameLibrary.Api.Models
 {
     public class UpsertCopyRequestModel
     {
         public string Title { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must provide a Copy ID")]
         public string LibraryID { get; set; }
         public bool? Winnable { get; set; }
         public int? CollectionID { get; set; }
