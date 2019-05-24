e42eeec81f9d8d664966e2508fda1086d2e23f24
jjh456 <jjh456@gmail.com>
2019-05-24 08:28:22 -0500

    Sql scripts updated

--- SqlScripts/DeleteNonMiniMarketData.sql
+++ SqlScripts/DeleteNonMiniMarketData.sql
@@ -1,12 +1,49 @@
---USE GeekwayLibrary
+--USE Test
 USE Library
 
--- Work way up from the bottom of dependencies like you
--- originally suggested. Delete in order of joins: rating, player, play, etc
-SELECT * -- To delete instead: DELETE r
+DECLARE @badgeIdsToKeep table (Id nvarchar(max))
+INSERT INTO @badgeIdsToKeep 
+	VALUES ('8675309'), ('99123')
+
+DELETE r
+--SELECT *
 FROM dbo.Ratings r
 INNER JOIN dbo.Players player on player.ID = r.ID
 INNER JOIN dbo.Plays play on play.ID = player.Play_ID
 INNER JOIN dbo.Checkouts ch on ch.ID = play.ID
 INNER JOIN dbo.Attendees a on a.ID = ch.Attendee_ID
-WHERE a.BadgeID NOT in ('8675309', '99123')
+WHERE a.BadgeID NOT in (SELECT Id from @badgeIdsToKeep)
+
+DELETE player
+--SELECT *
+FROM dbo.Players player
+INNER JOIN dbo.Plays play on play.ID = player.Play_ID
+INNER JOIN dbo.Checkouts ch on ch.ID = play.ID
+INNER JOIN dbo.Attendees a on a.ID = ch.Attendee_ID
+WHERE a.BadgeID NOT in (SELECT Id from @badgeIdsToKeep)
+
+DELETE play
+--SELECT *
+FROM dbo.Plays play
+INNER JOIN dbo.Checkouts ch on ch.ID = play.ID
+INNER JOIN dbo.Attendees a on a.ID = ch.Attendee_ID
+WHERE a.BadgeID NOT in (SELECT Id from @badgeIdsToKeep)
+
+UPDATE cop
+SET cop.CurrentCheckout_ID = NULL
+--SELECT *
+FROM dbo.Copies cop
+INNER JOIN dbo.Checkouts ch on ch.Copy_ID = cop.ID
+INNER JOIN dbo.Attendees a on a.ID = ch.Attendee_ID
+WHERE a.BadgeID NOT in (SELECT Id from @badgeIdsToKeep)
+
+DELETE ch
+--SELECT *
+FROM dbo.Checkouts ch
+INNER JOIN dbo.Attendees a on a.ID = ch.Attendee_ID
+WHERE a.BadgeID NOT in (SELECT Id from @badgeIdsToKeep)
+
+DELETE a
+--SELECT *
+FROM dbo.Attendees a
+WHERE a.BadgeID NOT in (SELECT Id from @badgeIdsToKeep)
